48672edca88cdd4b2312716031f7093674e659c4
noriyoshi.tanaka <aburagirio@gmail.com>
2024-03-02 15:55:32 +0900

    (update) Hiltを導入した

--- app/src/main/java/com/example/retrofitExample/MainActivity.kt
+++ app/src/main/java/com/example/retrofitExample/MainActivity.kt
@@ -11,39 +11,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.retrofitExample.interfaces.JsonPlaceHolderAPI
 import com.example.retrofitExample.ui.theme.RetrofitTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+import javax.inject.Inject
 
 private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     /**
-     * Retrofitのインスタンス
-     * - 接続先は https://jsonplaceholder.typicode.com/
-     * - converterとしてGsonConverterFactoryを与えている
-     */
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("https://jsonplaceholder.typicode.com/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    /**
      * JsonPlaceHolderAPIのインスタンス
      *
      * https://jsonplaceholder.typicode.com/ から記事を取得する
      *
+     * - converterとしてGsonConverterFactoryを与えている
      */
-    private val jsonPlaceHolderAPI = retrofit.create<JsonPlaceHolderAPI>()
+    @Inject
+    lateinit var jsonPlaceHolderAPI: JsonPlaceHolderAPI
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.Default).launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                /**
+                 * Resumeすると実行する
+                 */
+                launch(Dispatchers.IO) {
+                    val postList = jsonPlaceHolderAPI.getPosts()
+                    val text = "postList size is ${postList.size}"
+                    Log.d(TAG, "getPost: $text")
+                    Log.d(TAG, "getPost: ${postList[0].text}")
+                }
+            }
+        }
 
         setContent {
             RetrofitTheme {
@@ -55,17 +62,6 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        lifecycleScope.launch {
-            val postList = jsonPlaceHolderAPI.getPosts()
-            val text = "postList size is ${postList.size}"
-            Log.d(TAG, "onResume: $text")
-            Log.d(TAG, "onResume: ${postList[0].text}")
         }
     }
 }
